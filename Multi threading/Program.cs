7ecab9eff77b6293f27e573dd020723ae4f9e702
prem chand <premchandgurram19@gmail.com>
2026-03-12 17:33:56 +0530

    Add project files.

---
+++ Multi threading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public class File<T>
+{
+    public T ReadFile(string path)
+    {
+        try
+        {
+            string content = File.ReadAllText(path);
+            return (T)Convert.ChangeType(content, typeof(T));
+        }
+        catch(FileNotFoundException)
+        {
+            Console.WriteLine($"Error File not found");
+            return default(T);
+        }
+    }
+    public void WriteFile(string path, T obj)
+    {
+        File.WriteAllText(path, obj.ToString());
+        Console.WriteLine($"Data written to {path}");
+    }
+}
+public class Program
+{
+    public static async Task Main()
+    {
+        File<string> file1 = new File<string>();
+        Task<string> readTask = Task.Run(() => { return file1.ReadFile("input.txt"); });
+        Task writeTask = readTask.ContinueWith(task =>
+        {
+            if (task.Result != null)
+            {
+                string processed = task.Result.ToUpper();
+                file1.WriteFile("result.txt", processed);
+            }
+        });
+        await Task.WhenAll(readTask, writeTask);
+        Console.WriteLine("File Processing completed");
+    }
+}
