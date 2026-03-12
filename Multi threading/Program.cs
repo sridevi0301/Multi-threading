a5e615196e3a69ecaa97fe074ee5c2868a2f6d71
prem chand <premchandgurram19@gmail.com>
2026-03-12 18:57:45 +0530

    multi

--- Multi threading/Program.cs
+++ Multi threading/Program.cs
@@ -1,31 +1,33 @@
 ﻿using System;
-public class File<T>
+public class FileHandler<T>
 {
     public T ReadFile(string path)
     {
         try
         {
-            string content = File.ReadAllText(path);
+            string content = System.IO.File.ReadAllText(path);
             return (T)Convert.ChangeType(content, typeof(T));
         }
-        catch(FileNotFoundException)
+        catch (FileNotFoundException)
         {
-            Console.WriteLine($"Error File not found");
+            Console.WriteLine("Error File not found");
             return default(T);
         }
     }
+
     public void WriteFile(string path, T obj)
     {
-        File.WriteAllText(path, obj.ToString());
+        System.IO.File.WriteAllText(path, obj.ToString());
         Console.WriteLine($"Data written to {path}");
     }
 }
+
 public class Program
 {
     public static async Task Main()
     {
-        File<string> file1 = new File<string>();
-        Task<string> readTask = Task.Run(() => { return file1.ReadFile("input.txt"); });
+        FileHandler<string> file1 = new FileHandler<string>();
+        Task<string> readTask = Task.Run(() => { return file1.ReadFile("C:\\Users\\chand\\source\\repos\\Multi threading\\Multi threading\\result.txt"); });
         Task writeTask = readTask.ContinueWith(task =>
         {
             if (task.Result != null)
